7b11c869d42eed70fc40ef7892f8b284f3656b85
Ricardo Alvarez <ralvarezcm@gmail.com>
2022-05-08 04:24:38 -0500

    feat: add missing files for service coms

--- sentiment_api_service/src/routes/sentiment/analysis.ts
+++ sentiment_api_service/src/routes/sentiment/analysis.ts
@@ -1,12 +1,20 @@
 import { Request, Response, Router } from 'express';
+import Redis from 'ioredis';
 
 const route = Router();
+const redis = new Redis({
+  host: process.env.REDIS_HOST || 'redis',
+  port: parseInt(process.env.REDIS_PORT || '6379'),
+  db: 0
+});
 
 export function registerAnalysisRoutes(router: Router) {
   router.use('/analysis', route);
 
   route.post('/', (req: Request, res: Response) => {
-    const { tweets } = req.body;
-    res.send(tweets);
+    const { username } = req.body;
+    redis.xadd('events', '*', 'username', 'prueba');
+    console.log('Published event to redis.');
+    res.send('success');
   });
 }

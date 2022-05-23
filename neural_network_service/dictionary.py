4bdac95f887216ef51ede62e25742646da7083f3
Ricardo Alvarez <ralvarezcm@gmail.com>
2022-05-22 22:52:08 -0500

    feat: expanded dictionary & modified model

---
+++ neural_network_service/dictionary.py
@@ -0,0 +1,78 @@
+import nltk
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+
+palabras_stop = stopwords.words('spanish');
+
+puntuaciones = [',', '.', '!', '?', ';', '-', '..', '...', '....']
+
+palabras_negativas = [
+'Abanto', 'Abrazafarolas', 'Adufe', 'Alcornoque', 'Alfeñique', 'Andurriasmo', 'Arrastracueros', 'Artabán',
+'Artaban', 'Atarbán', 'Atarban', 'Atarre', 'Baboso', 'Barrabás', 'Barriobajero', 'Bebecharcos', 'Bellaco',
+'Belloto', 'Berzotas', 'Besugo', 'Bobalicón', 'Bocabuzón', 'Bocachancla', 'Bocallanta', 'Boquimuelle',
+'Borrico', 'Botarate', 'Brasas', 'Cabestro', 'Cabezaalberca', 'Cabezabuque', 'Cachibache', 
+'Cafre', 'Cagalindes', 'Cagarruta', 'Calambuco', 'Calamidad', 'Caldúo', 'Calientahielos', 'Calzamonas', 'Cansalmas', 
+'Cantamañanas', 'Capullo', 'Caracaballo', 'Caracartón', 'Caraculo', 'Caraflema', 'Carajaula', 'Carajote', 'Carapapa', 'Carapijo', 
+'Cazurro', 'Cebollino', 'Cenizo', 'Cenutrio', 'Ceporro', 'Cernícalo', 'Charrán', 'Chiquilicuatre', 'Chirimbaina', 'Chupacables', 'Chupasangre', 
+'Chupóptero', 'Cierrabares', 'Cipote', 'Comebolsas', 'Comechapas', 'Comeflores', 'Comestacas', 'Cretino', 'Cuerpoescombro', 'Culopollo', 
+'Descerebrado', 'Desgarracalzas', 'Dondiego', 'Donnadie', 'Echacantos', 'Ejarramantas', 'Energúmeno', 'Esbaratabailes', 'Escolimoso', 
+'Escornacabras', 'Estulto', 'Fanfosquero', 'Fantoche', 'Fariseo', 'Filimincias', 'Foligoso', 'Fulastre', 'Ganapán', 'Ganapio', 'Gandúl', 
+'Gañán', 'Gaznápiro', 'Gilipuertas', 'Giraesquinas', 'Gorrino', 'Gorrumino', 'Guitarro', 'Gurriato', 'Habahelá', 'Huelegateras', 'Huevón', 
+'Lamecharcos', 'Lameculos', 'Lameplatos', 'Lechuguino', 'Lerdo', 'Letrín', 'Lloramigas', 'Longanizas', 'Lumbreras', 'Maganto', 'Majadero', 
+'Malasangre', 'Malasombra', 'Malparido', 'Mameluco', 'Mamporrero', 'Manegueta', 'Mangarrán', 'Mangurrián', 'Mastuerzo', 'Matacandiles', 
+'Meapilas', 'Melón', 'Mendrugo', 'Mentecato', 'Mequetrefe', 'Merluzo', 'Metemuertos', 'Metijaco', 'Mindundi', 'Morlaco', 'Morroestufa', 
+'Muerdesartenes', 'Orate', 'Ovejo', 'Pagafantas', 'Palurdo', 'Pamplinas', 'Panarra', 'Panoli', 'Papafrita', 'Papanatas', 'Papirote', 'Paquete', 
+'Pardillo', 'Parguela', 'Pasmarote', 'Pasmasuegras', 'Pataliebre', 'Patán', 'Pavitonto', 'Pazguato', 'Pecholata', 'Pedorro', 'Peinabombillas', 
+'Peinaovejas', 'Pelagallos', 'Pelagambas', 'Pelagatos', 'Pelatigres', 'Pelazarzas', 'Pelmazo', 'Pelele', 'Pelma', 'Percebe', 'Perrocostra', 
+'Perroflauta', 'Peterete', 'Petimetre', 'Picapleitos', 'Pichabrava', 'Pillavispas', 'Piltrafa', 'Pinchauvas', 'Pintamonas', 'Piojoso', 
+'Pitañoso', 'Pitofloro', 'Plomo', 'Pocasluces', 'Pollopera', 'Quitahipos', 'Rastrapajo', 'Rebañasandías', 'Revientabaules', 'Ríeleches', 
+'Robaperas', 'Sabandija', 'Sacamuelas', 'Sanguijuela', 'Sinentraero', 'Sinsustancia', 'Sonajas', 'Sonso', 'Soplagaitas', 'Soplaguindas', 
+'Sosco', 'Tagarote', 'Tarado', 'Tarugo', 'Tiralevitas', 'Tocapelotas', 'Tocho', 'Tolai', 'Tontaco', 'Tontucio', 'Tordo', 'Tragaldabas', 
+'Tuercebotas', 'Tunante', 'Zamacuco', 'Zambombo', 'Zampabollos', 'Zamugo', 'Zángano', 'Zarrapastroso', 'Zascandil', 'Zopenco', 'Zoquete',
+'Zote', 'Zullenco', 'Zurcefrenillos', 'No', 'cobarde', 'tibio', 'mk', 'wuevon', 'wvon', 'hvon', 'marica', 'maricon', 'estupido', 'estupida',
+'culebrero', 'jibaro', 'quitarnosla', 'quitar', 'amenaza', 'terrorista', 'guerrillas', 'disparate', 'hampon', 'narcoterrorista',
+'siniestra', 'grotesca', 'malvada', 'malvado', 'pobreza', 'tirania', 'ruina', 'oscuridad', 'sapos', 'monopolio', 'dañar',
+'grave', 'mal', 'conflicto', 'revictimizante', 'agredidas', 'hp', 'envidia', 'hpta', 'nervioso', 'preocupado', 'derrota', 'falsos',
+'malo', 'tonta', 'tonto', 'hijueputa', 'bufón', 'bufon', 'chatarra', 'matar', 'malos', 'vicioso', 'narcotraficante', 'violencia', 'mentir',
+'titere', 'caga', 'narco', 'payasín', 'susto', 'robar', 'criminales', 'mentira', 'corrupto', 'paramilitar', 'culo', 'prepago', 'uribestia',
+'resentida', 'rogados', 'inhabilitado', 'golpes', 'traquetos', 'ilegal', 'panico', 'mafias', 'violando', 'violación', 'violacion', 'ignorante',
+'chismoso', 'complice', 'chimbo', 'pendejadas', 'fracasado', 'fracasada', 'enfermedad', 'muerte', 'narcotráfico', 'asesinos', 'destrucción',
+'hambre', 'mierda', 'mrd', 'bobo', 'pobres', 'felices', 'secuestros', 'secuestro', 'desaparecidos', 'insoportables', 'insoportable', 'pésimo', 'pesimo',
+'populista', 'payaso', 'enemigo', 'hipocrita', 'hipócrita', 'bruto', 'bobos', 'paraco', 'paracos', 'bandido', 'perdedor', 'desafortunadamente',
+'contaminaron', 'lastima', 'insultar', 'incongruente', 'maltrato', 'terror', 'delincuencia', 'delincuente', 'loco', 'loca', 'descaro', 'descarada',
+'descarado', 'empeoro', 'empeorar', 'empeorando', 'subdito', 'subditos', 'mala', 'nada', 'putas', 'puta', 'estupidos', 'estúpidos',
+'lagarto', 'lagartear', 'bandidos', 'obsesión', 'obsesion', 'narcotraficantes', 'destruido', 'destruir', 'charlatanes', 'ratas',
+'undir', 'daño', 'dano', 'pesimas', 'pesimos', 'costosa', 'estafa', 'prepagos', 'golpe', 'atarbanes', 'arcaico', 'monda', 'verga', 'huevo', 'huevon',
+'gonorrea', 'chimba', 'perra', 'peor', 'desgraciado', 'desgraciada', 'desgracia', 'desagradable', 'basura', 'pirobo', 'piroba',
+'torcido', 'ladrones', 'ladron', 'vulgar', 'patan', 'hipócrita', 'falso', 'desleal', 'farsa', 'engañar', 'engaño', 'guerrillera', 'guerrilleras',
+'hjpta', 'qlo', 'descabezado', 'curtido', 'deslealtad', 'demencia', 'demente', 'vergüenza', 'miedo', 'desesperados', 'desesperado', 'sinvergüenza'
+]
+
+palabras_positivas = [
+'abrazo', 'admirable', 'agraciado', 'agradable', 'agradecido', 'amigo', 'amistad', 'amor', 'apasionado', 
+'aprecio', 'aprobacion', 'aprobación', 'apropiado', 'bella', 'bello', 'bendecido', 'beso', 'bien', 'bonita', 'bonito', 
+'calificado', 'calma', 'capacitado', 'cariñoso', 'carisma', 'celebracion', 'celebración', 'compasion', 'compasión', 
+'comprensivo', 'confiable', 'conveniente', 'curioso', 'decente', 'delicada', 'delicado', 'destreza', 'dichoso', 'digno', 
+'dios', 'disciplina', 'divina', 'divino', 'dulce', 'dulzura', 'educada', 'educado', 'efectiva', 'efectivo', 'eficaz', 'eficiente', 
+'efusiva', 'efusivo', 'ejemplar', 'elegancia', 'elegante', 'elocuente', 'emocionada', 'emocionado', 'empatia', 'empatico', 'empatico', 
+'empatizar', 'enamorada', 'enamorado', 'encantada', 'encantado', 'energetica', 'energetico', 'equitativa', 'equitativo', 'especial', 
+'espectacular', 'esperanza', 'espiritu', 'estelar', 'estimulante', 'excelente', 'excelentes', 'excepcional', 'exito', 'exitosa', 'exitoso', 'fabuloso', 
+'facil', 'facilidad', 'factible', 'familia', 'fantastico', 'fascinante', 'felicitar', 'felicito', 'feliz', 'fortaleza', 'futuro', 'ganador', 'generosa', 
+'generoso', 'genial', 'gracia', 'gracias', 'gratitud', 'hermosa', 'hermoso', 'ideal', 'idolatrar', 'imparcial', 'impresionante', 
+'incondicional', 'inolvidable', 'integral', 'inteligencia', 'inteligente', 'intrigante', 'joven', 'juicioso', 'justa', 'justo', 
+'juventud', 'leal', 'lealtad', 'legal', 'legendario', 'lider', 'madurez', 'magico', 'magnifica', 'magnifico', 'maravilloso', 
+'memorable', 'merecedor', 'meticuloso', 'milagro', 'modesta', 'modesto', 'monumental', 'notable', 'oportunidad', 'optima', 
+'optimismo', 'optimista', 'optimo', 'orden', 'ordenada', 'ordenado', 'orgullo', 'orgulloso', 'ostentoso', 'placer', 'positivo', 
+'querido', 'querida', 'sabiduria', 'sabio', 'sabroso', 'saludable', 'salvacion', 'salvación', 'santa', 'santo', 'satisfecha', 
+'satisfecho', 'sencilla', 'sencillo', 'sensacional', 'serena', 'serenidad', 'sereno', 'si', 'sí', 'sinceramente', 'sincero', 
+'sincero', 'solidaria', 'solidario', 'solucion', 'solución', 'sonrisa', 'sorprendente', 'sostenible', 'super', 'talento', 'talentosa', 
+'talentoso', 'tesoro', 'tolerancia', 'tolerante', 'trabajador', 'trabajadora', 'triunfal', 'triunfo', 'trofeo', 'unidad', 'util', 'útil', 
+'valentia', 'valentía', 'valiente', 'valioso', 'valor', 'ventaja', 'vigor', 'visionario', 'virtuoso', 'virtud', 'vigorizante', 'victorioso', 
+'vivaz', 'afinidad', 'vida', 'amo', 'fuerza', 'fortaleza', 'dignidad', 'bendición', 'unidos', 'apoyo', 'sueño', 'anhelado', 'mejor', 'inclusión',
+'inclusion', 'sana', 'salud', 'salva', 'correctos', 'correcto', 'animo', 'bueno', 'conviccion', 'convicción', 'merecemos', 'transparente', 'quiero',
+'humildad', 'inteligencia', 'amas', 'honestidad', 'sensata', 'encanta', 'buena', 'buenas', 'gran', 'dedicacion', 'dedicación',
+'divinos', 'divinas', 'belleza', 'perfectas', 'perfectos', 'resistente', 'gusta', 
+]
+
+print ('Palabras negativas:', len(palabras_negativas))
+print ('Palabras positivas:', len(palabras_positivas))
--- neural_network_service/model.py
+++ neural_network_service/model.py
@@ -1,4 +1,3 @@
-from gc import callbacks
 import numpy as np
 import keras
 from keras.utils import pad_sequences
@@ -9,11 +8,11 @@ from utils import load_encoded_data, parseInputs
 
 
 class Model:
-    max_words, batch_size, maxlen, epochs = 2400, 64, 50, 50
-    embedding_dims, filters, kernel_size, hidden_dims = 12, 60, 2, 30
+    max_words, batch_size, maxlen, epochs = 2400, 32, 70, 100
+    embedding_dims, filters, kernel_size, hidden_dims = 5, 25, 5, 20
 
     # Generate split training and testing data (80% training, 20% testing)
-    x_train, x_test, y_train, y_test = load_encoded_data(data_split=0.8)
+    x_train, x_test, y_train, y_test = load_encoded_data(data_split=0.85)
 
     # Determine the number of categories + default(i.e. sentence types)
     num_classes = np.max(y_train) + 1
@@ -25,6 +24,17 @@ class Model:
     # Pad the input vectors to ensure a consistent length
     x_train = pad_sequences(x_train, maxlen=maxlen, padding='post')
     x_test = pad_sequences(x_test, maxlen=maxlen, padding='post')
+
+    early_stopping_monitor = EarlyStopping(
+        monitor='val_loss',
+        min_delta=0.001,
+        patience=10,
+        verbose=0,
+        mode='min',
+        restore_best_weights = True
+    )
+    mcp_save = ModelCheckpoint('./best_models/md.ep{epoch:02d}-loss{val_loss:.2f}-acc{val_accuracy:.2f}.hdf5', save_best_only=True, monitor='val_loss', mode='min')
+    reduce_lr_loss = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=7, verbose=1, min_delta=0.0001, mode='min')
 
     print(x_train)
 
@@ -56,37 +66,32 @@ class Model:
         self.trainModel()
 
     def trainModel(self):
-        early_stopping_monitor = EarlyStopping(
-            monitor='val_loss',
-            min_delta=0,
-            patience=50,
-            verbose=0,
-            mode='min',
-            restore_best_weights = True
-        )
-        mcp_save = ModelCheckpoint('.mdl_wts.hdf5', save_best_only=True, monitor='val_loss', mode='min')
-        reduce_lr_loss = ReduceLROnPlateau(monitor='val_loss', factor=0.001, patience=10, verbose=1, min_delta=0, mode='min')
-        print('Training model...')
-        # "Fit the model" (train model), using training data (80% of datset)
+        cbs = [self.early_stopping_monitor, self.mcp_save, self.reduce_lr_loss]
         self.model.fit(self.x_train, self.y_train, batch_size=self.batch_size,
-                       epochs=self.epochs, validation_data=(self.x_test, self.y_test), callbacks=[mcp_save, reduce_lr_loss])
-        print('Finished training model')
+                       epochs=self.epochs, validation_data=(self.x_test, self.y_test), callbacks=cbs)
         score = self.model.evaluate(self.x_test, self.y_test, batch_size=self.batch_size)
         print(self.model.metrics_names)
         print(score)
 
+
 if __name__ == '__main__':
     classifier = Model()
     x_predictions = parseInputs([
-    'psa Este es un tweet de prueba',
-    'Ese candidato es un paraco narcotraficante', 
-    'Excelentes noticias que buena persona',
-    'malparido paraco no tiene ni idea de lo que le hace al pais, el y sus subditos',
-    'ese canelo es un pesimo boxeador, no sabe nada del deporte', 
-    'que buenas noticias, te doy las gracias por eso',
-    'Que buena persona eres, te felicito por tu labor y por tu dedicacion, te doy las gracias por tu excelente trabajo',
-    'este es un tweet de prueba, ese candidato es un malparido porque no sabe que esta haciendo con el pais. Esta empeorando la situacion economica de todos los colombianos y tiene el descaro de decir que le va hacer bien al pais cuando todo lo que ha hecho ha sido pesimo y de mala calidad'
+    "Este es un tweet de prueba",
+    "Ese candidato es un paraco narcotraficante", 
+    "Excelentes noticias que buena persona",
+    "malparido paraco no tiene ni idea de lo que le hace al pais, el y sus subditos",
+    "ese canelo es un pesimo boxeador, no sabe nada del deporte", 
+    "que buenas noticias, te doy las gracias por eso",
+    "Que buena persona eres, te felicito por tu labor y por tu dedicacion, te doy las gracias por tu excelente trabajo",
+    "este es un tweet de prueba, ese candidato es un malparido porque no sabe que esta haciendo con el pais. Esta empeorando la situacion economica de todos los colombianos y tiene el descaro de decir que le va hacer bien al pais cuando todo lo que ha hecho ha sido pesimo y de mala calidad"
     ])
-    x_predictions = pad_sequences(x_predictions, maxlen=50, padding='post')
+    x_predictions = pad_sequences(x_predictions, maxlen=70, padding='post')
     prediction = classifier.model.predict(x_predictions)
-    print(prediction.argmax(axis=-1))
+
+    
+    savedModel = keras.models.load_model('./best_models/md.ep23-loss0.60-acc0.82.hdf5')
+    savedPred = savedModel.predict(x_predictions)
+
+    print( 'New predictions', prediction.argmax(axis=-1))
+    print( 'Saved model', savedPred.argmax(axis=-1))
